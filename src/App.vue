0414c0bc89a2055a56a51e4467a80e8792ba215c
evont <evontgoh@foxmail.com>
2018-07-30 23:00:16 +0800

    [feat]-init template

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  position: relative;
+}
+html, body {
+  height: 100%;
+  overflow: hidden;
+  font-size: 14px;/* no */
+}
+a {
+  text-decoration: none;
+  color: #2c3e50;
+}
+#app {
+  height: 100%;
+  max-width: 480px;/* no */
+  min-width: 320px;/* no */
+  margin: 0 auto;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/pages/Static.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="resume">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Static',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
