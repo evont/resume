80e6ea8981ca85d63e6b2a90c3f045a3238f7e65
Evont Ng <evontng@EvontNg.local>
2018-08-05 20:37:58 +0800

    [feat] - change file structure & add frame

---
+++ src/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="m-content"></div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
